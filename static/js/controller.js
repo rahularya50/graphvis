c0871dd80cca2bdee398fba62ccd7cca6d968ef5
rahularya <rahularya50@gmail.com>
2018-08-14 21:45:01 -0700

    Began implementing data viz GUI to complete tonight

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,11 +1,12 @@
 $("#graphdata").submit((e) => {
     $.ajax("/process_graph", {
         method: "post",
-        dataType: "json",
+        dataType: "json"
         data: $("#graphdata").serialize(),
         success: (data, status, xhr) => {
             console.log(data);
             draw_graph(data["graph"]);
+            load_data(data[""])
         }
     });
     return false;
